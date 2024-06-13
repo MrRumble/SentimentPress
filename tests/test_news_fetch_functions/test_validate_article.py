89374ec140f666e1412626564f5a515f7df0f941
James Rumble <jamesrumble@live.com>
2024-06-13 09:19:16 +0100

    Testing: More testing completed on news_fetch_functions

--- tests/test_news_fetch_functions/test_validate_article.py
+++ tests/test_news_fetch_functions/test_validate_article.py
@@ -14,3 +14,6 @@ def test_validate_article_sentiment_zero_false():
 
 def test_validate_article_none_title_description_false():
     assert validate_article(None, None, 0.8) == False
+
+def test_validate_article_removed_returns_false():
+    assert validate_article('[Removed]', '[Removed]', 0.8) == False
