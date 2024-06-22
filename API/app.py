397ae7a6b4940d3f8f6a0ff0e2cd656028cd08f7
James Rumble <jamesrumble@live.com>
2024-06-22 16:58:32 +0100

    Test Route Added

--- API/app.py
+++ API/app.py
@@ -1,5 +1,17 @@
 from lib.news_fetch_functions import *
+from flask import Flask
+from flask_cors import CORS
 
-df = fetch_and_process_query('trump', 100)
 
-print(df)
+load_dotenv()
+# Create a new Flask app
+app = Flask(__name__)
+CORS(app)
+
+from routes.test_route import *
+
+if __name__ == '__main__':
+    app.run(debug=True, port=int(os.environ.get('PORT', 5002)))
+# df = fetch_and_process_query('trump', 100)
+
+# print(df)
---
+++ API/routes/test_route.py
@@ -0,0 +1,11 @@
+from flask import jsonify
+from app import app
+from lib.news_fetch_functions import *
+
+#This is a test route to see if I can pass data across to Back-end server. 
+@app.route("/test", methods=["GET"])
+def test():
+    df = fetch_and_process_query('Trump', 100)
+    json_data = df.to_json(orient='records')
+    return jsonify(json_data)
+    
