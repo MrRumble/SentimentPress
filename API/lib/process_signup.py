7c97dda7fd24a115fd865edc6973c0ef110f700e
Ben Sullivan <bensullivan2002@gmail.com>
2024-07-19 13:49:27 +0100

    Signup completed

--- API/lib/process_signup.py
+++ API/lib/process_signup.py
@@ -6,6 +6,10 @@ class SignupProcessor:
     def __init__(self):
         self.database_connection = DatabaseConnection()
 
+    def check_email_exists(self, email):
+        db_connection = self.database_connection.get_database()
+        return db_connection['users'].find_one({"email": email}) is not None
+
     def insert_user(self, firstname, lastname, email, password):
         user = {
             "firstname": firstname,
@@ -16,7 +20,3 @@ class SignupProcessor:
 
         db_connection = self.database_connection.get_database()
         db_connection["users"].insert_one(user)
-
-    def check_email_exists(self, email):
-        db_connection = self.database_connection.get_database()
-        return db_connection['users'].find_one({"email": email}) is not None
--- frontend/src/components/signup/Validator.js
+++ frontend/src/components/signup/Validator.js
@@ -1,12 +1,53 @@
 class Validator {
 
-    static validateEmail (email) {
+    static validateEmail(email) {
         // Regular expression for email validation
         const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
         // Test the email against the regex
         return emailRegex.test(String(email).toLowerCase());
     }
+
+    static validatePassword(password) {
+        // Initialize an object to store validation results
+        const result = {
+            isValid: true,
+            errors: []
+        };
+
+        // Check length
+        if (password.length < 8) {
+            result.isValid = false;
+            result.errors.push("Password must be 8 characters or longer");
+        }
+
+        // Check for lowercase letter
+        if (!/[a-z]/.test(password)) {
+            result.isValid = false;
+            result.errors.push("Password must include at least one lowercase letter");
+        }
+
+        // Check for uppercase letter
+        if (!/[A-Z]/.test(password)) {
+            result.isValid = false;
+            result.errors.push("Password must include at least one uppercase letter");
+        }
+
+        // Check for number
+        if (!/\d/.test(password)) {
+            result.isValid = false;
+            result.errors.push("Password must include at least one number");
+        }
+
+        // Check for special character
+        if (!/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password)) {
+            result.isValid = false;
+            result.errors.push("Password must include at least one special character");
+        }
+
+        return result;
+    }
 }
+
 
 export default Validator;
