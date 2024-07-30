f80eec27b143dee877f984a2a55e838cd06d7466
Rumble <rumble@Admins-MacBook-Pro.local>
2024-07-30 15:07:58 +0100

    redis_connection and redis_db updated to include test_db option

--- api/lib/redis_manager.py
+++ api/lib/redis_manager.py
@@ -1,11 +1,11 @@
 from .redis_connection import RedisConnection
 
 
-def add_to_blacklist(jti, expires_at=30):  # Expiry time is in SECONDS
-    with RedisConnection() as red_conn:
+def add_to_blacklist(jti, expires_at=30, use_test_db=False):  # Expiry time is in SECONDS
+    with RedisConnection(use_test_db=use_test_db) as red_conn:
         red_conn.set(jti, '', ex=expires_at)
 
 
-def is_blacklisted(jti):
-    with RedisConnection() as red_conn:
+def is_blacklisted(jti, use_test_db=False):
+    with RedisConnection(use_test_db=use_test_db) as red_conn:
         return red_conn.get(jti) is not None
